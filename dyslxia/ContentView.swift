1f5d9ac6b79561b8b7f3da04d15cc9aa4511cc6e
Veit Progl <mail@veit.dev>
2019-06-04 14:58:48 -0700

    Initial Commit

---
+++ dyslxia/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  dyslxia
+//
+//  Created by test on 04.06.19.
+//  Copyright © 2019 veit.dev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
